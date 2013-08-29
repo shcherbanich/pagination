2ecbce1a8f8c2e9babb4a864a9b331600650f8f1
Philip <filippf@bk.ru>
2013-08-29 12:31:59 +0400

    Update pagination.js

--- pagination.js
+++ pagination.js
@@ -1,7 +1,7 @@
 (function($){
 	var methods = {
 		build:function(){
-			var sep,o=this.data('pagination'),max=this.pagination('_getPage',o.items),elements=(o.templatePrev?'<li '+(o.active==1?'class="disabled"':'')+'>'+o.templatePrev+'</li>':''),start=(c=(sep=(o.active-o.show/2|0)+1,o.hideUn?(sep>=1?(max-sep<o.show?max-o.show+1:sep):1):1),c?c:1),stop=o.hideUn?(o.show+start<=max?o.show+start-1:max):max;o.useTplHidden&&start!=1?(o.templateHidden?elements+='<li class="disabled">'+o.templateHidden+'</li>':0):0;for(i=start;i<=stop;i++)disabled=o.disabled.indexOf(i)!=-1?1:0,elements+='<li'+(o.active==i?' class="active"':disabled?' class="disabled"':'')+'>'+(o.active==i?o.templateActive.replace(/\$num/g,i):(disabled?o.templateDisabled.replace(/\$num/g,i):o.templateLinks.replace(/\$num/g,i)))+'</li>';o.useTplHidden&&stop!=max?(o.templateHidden?elements+='<li class="disabled">'+o.templateHidden+'</li>':0):0;o.templateNext?elements+='<li '+(o.active==max?'class="disabled"':'')+'>'+o.templateNext+'</li>':0;return this.children('ul').empty().append(elements).parent();
+			var sep,o=this.data('pagination'),max=this.pagination('_getPage',o.items),elements=(o.templatePrev?'<li id="prev"'+(o.active==1?'class="disabled"':'')+'>'+o.templatePrev+'</li>':''),start=(c=(sep=(o.active-o.show/2|0)+1,o.hideUn?(sep>=1?(max-sep<o.show?max-o.show+1:sep):1):1),c?c:1),stop=o.hideUn?(o.show+start<=max?o.show+start-1:max):max;o.useTplHidden&&start!=1?(o.templateHidden?elements+='<li id="hiddenPrev"class="disabled">'+o.templateHidden+'</li>':0):0;for(i=start;i<=stop;i++)disabled=o.disabled.indexOf(i)!=-1?1:0,elements+='<li class="act'+(o.active==i?' active':disabled?' disabled':'')+'"data-num="'+i+'">'+(o.active==i?o.templateActive.replace(/\$num/g,i):(disabled?o.templateDisabled.replace(/\$num/g,i):o.templateLinks.replace(/\$num/g,i)))+'</li>';o.useTplHidden&&stop!=max?(o.templateHidden?elements+='<li id="hiddenNext"class="disabled">'+o.templateHidden+'</li>':0):0;o.templateNext?elements+='<li id="next"'+(o.active==max?'class="disabled"':'')+'>'+o.templateNext+'</li>':0;return this.children('ul').empty().append(elements).parent(),this.each(function(){var self=this;$(this).find('.act').bind('click',function(){o.activeAction(self,$(this).attr("data-num"))}).parent().find('#next').bind('click',function(){o.nextAction(self)}).parent().find('#prev').bind('click',function(){o.prevAction(self)}).parent().find('#hiddenPrev').bind('click',function(){o.hiddenPrevAction(self)}),$(this).find('#hiddenNext').bind('click',function(){o.hiddenNextAction(self)})});
 		}, 
 		display:function(page){
 			var o=this.data('pagination'),m=this.pagination('_getPage',o.items),max=o.pageItems*(page<=m?page:m);return {min:!o.pageItems?0:page?max-o.pageItems+1:0,max:!o.pageItems?0:(page>=m?o.items:max)} 
@@ -22,10 +22,10 @@
 			return this.pagination('active',this.pagination('_getPage',this.pagination('display').max));
 		},
 		nextPage:function(){
-			var o=this.data('pagination');return max=this.pagination('_getPage',o.items),act=(o.active+1<=max?++o.active:o.active),this.pagination('build');
+			var o=this.data('pagination'),max=this.pagination('_getPage',o.items);++o.active,o.active>max?--o.active:0;return this.pagination('build');
 		},
 		prevPage:function(){
-			var o=this.data('pagination');return max=this.pagination('_getPage',o.items),act=(o.active-1>=1?--o.active:o.active),this.pagination('build');
+			var o=this.data('pagination'),max=this.pagination('_getPage',o.items),act=(o.active-1>=1?--o.active:o.active);return this.pagination('build');
 		},
 		getActive:function(){
 			return this.data('pagination').active;
@@ -51,12 +51,17 @@
 										hideUn:true,
 										useTplHidden:true,
 										show:5,
-										templatePrev:'<a>«</a>',
-										templateNext:'<a>»</a>',
+										templatePrev:'<a href="#0">«</a>',
+										templateNext:'<a href="#0">»</a>',
 										templateHidden:'<a>...</a>',
 										templateActive : '<a href="#$num">$num</a>',
 										templateLinks : '<a href="#$num">$num</a>',
 										templateDisabled : '<a>$num</a>',
+										activeAction:function(self,number){$(self).pagination('active',number)},
+										prevAction:function(self){$(self).pagination('prevPage')},
+										nextAction:function(self){$(self).pagination('nextPage')},
+										hiddenPrevAction:function(self){$(self).pagination('setFirstPage')},
+										hiddenNextAction:function(self){$(self).pagination('setLastPage')},
 										theme:'default',
 										onInit:function(self){}
 							},object);this.data('pagination',settings),this.html('<ul></ul>'),this.pagination('_setTheme'),settings.onInit.call(this);return this;
